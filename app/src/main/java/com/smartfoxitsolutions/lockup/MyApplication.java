053a41969bcf921e31cd069fd9b830ed24d3cf9a
Raaja <raaja.012@gmail.com>
2016-12-22 21:17:33 +0530

    LockUp - Lock Final Fix

---
+++ app/src/main/java/com/smartfoxitsolutions/lockup/MyApplication.java
@@ -0,0 +1,21 @@
+package com.smartfoxitsolutions.lockup;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * Created by RAAJA on 20-12-2016.
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+      /*  if(LeakCanary.isInAnalyzerProcess(this)){
+            return;
+        }
+        LeakCanary.install(this); */
+    }
+}
