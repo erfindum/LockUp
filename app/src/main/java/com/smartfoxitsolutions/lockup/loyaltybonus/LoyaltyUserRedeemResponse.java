bdc738695d80704c006debde8ef212cda611a383
Raaja <raaja.012@gmail.com>
2017-02-22 18:48:34 +0530

    LockUp - Ads Redeem

---
+++ app/src/main/java/com/smartfoxitsolutions/lockup/loyaltybonus/LoyaltyUserRedeemResponse.java
@@ -0,0 +1,9 @@
+package com.smartfoxitsolutions.lockup.loyaltybonus;
+
+/**
+ * Created by RAAJA on 20-02-2017.
+ */
+
+public class LoyaltyUserRedeemResponse {
+    String code, msg, TotalPoint;
+}
---
+++ app/src/main/java/com/smartfoxitsolutions/lockup/loyaltybonus/dialogs/OnRequestRedeemListener.java
@@ -0,0 +1,11 @@
+package com.smartfoxitsolutions.lockup.loyaltybonus.dialogs;
+
+/**
+ * Created by RAAJA on 20-02-2017.
+ */
+
+public interface OnRequestRedeemListener {
+
+    void requestRedeem();
+    void requestCancelled();
+}
